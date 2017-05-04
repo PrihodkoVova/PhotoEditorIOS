14aef1787d681a1c57a4c32794539e8af8999942
Vladimir Prihodko <safip@zeoalliance.com>
2017-05-04 15:04:02 +0300

    Added empty project

---
+++ PhotoEditor/PhotoEditor/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PhotoEditor
+//
+//  Created by Vova on 5/4/17.
+//  Copyright © 2017 Vova Pryhodko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
